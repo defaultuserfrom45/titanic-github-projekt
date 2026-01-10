65628e585c08bc93ed42bcf522219a1c03eafd58
Rit Alija <ritalija7@gmail.com>
2026-01-10 17:07:31 +0100

    Kabinenmerkmale ergänzt und bereinigten Datensatz exportiert
    Added data cleaning steps for Cabin and Seite variables, including extraction of Deck and Cabin number. Also saved the cleaned dataset to a CSV file.

--- R/01_preprocessing.R
+++ R/01_preprocessing.R
@@ -25,10 +25,9 @@ data_raw$Embarked <- factor(data_raw$Embarked, levels = c("C","Q","S"), labels =
 
 data_raw$Pclass <- factor(data_raw$Pclass, levels = c(1,2,3), ordered = TRUE)
 
-# Kurzer Check
+# Kurzer Kontrollblick
 str(data_raw)
 summary(data_raw)
-
 
 # Fehlende Alterswerte imputieren (Median nach Anrede)
 
@@ -36,4 +35,43 @@ median_age <- tapply(data_raw$Age, data_raw$Anrede, median, na.rm = TRUE)
 
 idx_na <- is.na(data_raw$Age)
 data_raw$Age[idx_na] <- median_age[data_raw$Anrede[idx_na]]
+
+# Cabin: unbekannt -> NA, Deck und Seite extrahieren
+
+# Leere Cabin-Einträge auf NA setzen (falls als "" vorhanden)
+if ("Cabin" %in% names(data_raw)) {
+  data_raw$Cabin[data_raw$Cabin == ""] <- NA
+
+  # Deck: erster Buchstabe der Cabin (z.B. "C" aus "C85")
+  data_raw$Deck <- NA
+  idx_cabin <- !is.na(data_raw$Cabin)
+  data_raw$Deck[idx_cabin] <- substr(data_raw$Cabin[idx_cabin], 1, 1)
+
+  # Cabin-Nummer extrahieren (erste Zahl nach Buchstaben, z.B. 85 aus "C85")
+  cabin_num <- rep(NA_integer_, nrow(data_raw))
+  cabin_num[idx_cabin] <- suppressWarnings(as.integer(sub("^[A-Za-z]+", "", data_raw$Cabin[idx_cabin])))
+
+  # Seite: ungerade = Steuerbord, gerade = Backbord
+  data_raw$Seite <- NA
+  idx_num <- !is.na(cabin_num)
+  data_raw$Seite[idx_num] <- ifelse(cabin_num[idx_num] %% 2 == 1, "Steuerbord", "Backbord")
+  data_raw$Seite <- factor(data_raw$Seite, levels = c("Backbord", "Steuerbord"))
+
+  data_raw$Deck <- factor(data_raw$Deck)
+}
+
+# Unnötige Variablen entfernen (robust, je nach Spaltennamen)
+
+drop_cols <- c("PassengerID", "PassengerId", "Name", "Ticket", "Cabin")
+drop_cols <- drop_cols[drop_cols %in% names(data_raw)]
+data_clean <- data_raw[, !(names(data_raw) %in% drop_cols), drop = FALSE]
+
+# Bereinigten Datensatz speichern
+
+if (!dir.exists("data/processed")) dir.create("data/processed", recursive = TRUE)
+write.csv(data_clean, "data/processed/titanic_clean.csv", row.names = FALSE)
+
+# Abschluss-Check
+str(data_clean)
+summary(data_clean)
 
